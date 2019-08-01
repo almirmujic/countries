4d09019299ba505fffd97d176e510422fe8c2e66
Almir <contact.almir@gmail.com>
2019-08-01 15:23:09 -0500

    Fetch api data and set it to state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component() {
+class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { countries: [] };
+    this.state = { countries: [], isLoading: false };
+  }
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    fetch('https://restcountries.eu/rest/v2/all')
+      .then(res => res.json())
+      .then(data => this.setState({ countries: data, isLoading: false }));
   }
   render() {
-    return <div className="App" />;
+    const countries = this.state.countries.map((country, i) => (
+      <p key={i}>{country.name}</p>
+    ));
+    return <div className="App">{countries}</div>;
   }
 }
 
