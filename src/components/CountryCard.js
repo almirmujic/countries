f6a00d5ab64f532eb17823a93c61f272a05c056c
Almir <contact.almir@gmail.com>
2019-08-01 16:52:48 -0500

    Create style variables for certain elements of component

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -2,14 +2,45 @@ import React from 'react';
 
 const card = {
   boxSizing: 'border-box',
-  width: '350px'
+  width: '300px',
+  boxShadow: '3px 3px 8px -4px',
+  borderRadius: '15px'
 };
+
+const cImg = {
+  width: '100%',
+  height: '200px',
+  borderRadius: '15px 15px 0px 0px'
+};
+
+const details = {
+  paddingTop: '8px',
+  paddingLeft: '8px',
+  paddingBottom: '8px'
+};
+
+const unorderedList = {
+  listStylePosition: 'inside',
+  paddingTop: '8px'
+};
+
+// const header = {};
+
+// function to add commas inside of population
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
 
 export default function CountryCard(props) {
   return (
     <div style={card}>
-      <img style={{ maxWidth: '100%' }} src={props.img} alt={props.name} />
-      <p>{props.name}</p>
+      <img style={cImg} src={props.img} alt={props.name} />
+      <div style={details}>
+        <h4>{props.name}</h4>
+        <ul style={unorderedList}>
+          <li>Population: {numberWithCommas(props.population)}</li>
+        </ul>
+      </div>
     </div>
   );
 }
